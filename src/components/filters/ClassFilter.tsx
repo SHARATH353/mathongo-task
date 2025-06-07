a7a7547f85e9a14475efd65bcdee997a7c7d3bed
SHARATH353 <sharathkumarsk353@gmail.com>
2025-06-07 13:34:45 +0530

    fix: removed unused vars from filters for clean Vercel build

--- src/components/filters/ClassFilter.tsx
+++ src/components/filters/ClassFilter.tsx
@@ -1,19 +1,15 @@
 "use client";
 
 import { useChaptersStore } from "@/store/useChaptersStore";
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 export default function ClassFilter() {
-  const { filteredChapters, setFilters } = useChaptersStore();
+  const { setFilters, filteredChapters } = useChaptersStore();
 
   const classes = Array.from(new Set(filteredChapters.map((c) => c.class)));
 
   return (
-    <Select
-      onValueChange={(val) =>
-        setFilters({ class: val === "all" ? [] : [val] })
-      }
-    >
+    <Select onValueChange={(val) => setFilters({ class: val === "all" ? [] : [val] })}>
       <SelectTrigger className="w-[140px]">
         <SelectValue placeholder="Class" />
       </SelectTrigger>
--- src/components/filters/UnitFilter.tsx
+++ src/components/filters/UnitFilter.tsx
@@ -1,19 +1,15 @@
 "use client";
 
 import { useChaptersStore } from "@/store/useChaptersStore";
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 export default function UnitFilter() {
-  const { filteredChapters, setFilters } = useChaptersStore();
+  const { setFilters, filteredChapters } = useChaptersStore();
 
   const units = Array.from(new Set(filteredChapters.map((c) => c.unit)));
 
   return (
-    <Select
-      onValueChange={(val) =>
-        setFilters({ unit: val === "all" ? [] : [val] })
-      }
-    >
+    <Select onValueChange={(val) => setFilters({ unit: val === "all" ? [] : [val] })}>
       <SelectTrigger className="w-[140px]">
         <SelectValue placeholder="Unit" />
       </SelectTrigger>
